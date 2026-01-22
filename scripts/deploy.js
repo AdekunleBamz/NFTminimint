023d8a36a2641e7ac9bc029740465068a94cbf72
AdekunleBamz <bams.kunle@gmail.com>
2026-01-22 09:40:03 +0100

    chore(deploy): log deployer and contract config

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,15 @@
 async function main() {
+  const [deployer] = await ethers.getSigners();
   const NFTminimint = await ethers.getContractFactory("NFTminimint");
   const nftminimint = await NFTminimint.deploy();
 
   await nftminimint.deployed();
 
+  console.log("Deployer:", deployer.address);
   console.log("NFTminimint deployed to:", nftminimint.address);
+  console.log("mintFee:", (await nftminimint.mintFee()).toString());
+  console.log("maxSupply:", (await nftminimint.maxSupply()).toString());
+  console.log("maxPerWallet:", (await nftminimint.maxPerWallet()).toString());
 }
 
 main()
