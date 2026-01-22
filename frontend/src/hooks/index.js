2b7080e4828f6d36be63dab69c9662ec6df5526f
AdekunleBamz <bams.kunle@gmail.com>
2026-01-22 14:19:52 +0100

    feat(frontend): add utility hooks (useToast, useStorage, useMediaQuery)

--- frontend/src/hooks/index.js
+++ frontend/src/hooks/index.js
@@ -1,2 +1,12 @@
 export { useWallet } from './useWallet'
 export { useContract } from './useContract'
+export { useToast } from './useToast'
+export { useLocalStorage, useSessionStorage } from './useStorage'
+export { 
+  useMediaQuery, 
+  useIsMobile, 
+  useIsTablet, 
+  useIsDesktop,
+  usePrefersDarkMode,
+  usePrefersReducedMotion
+} from './useMediaQuery'
